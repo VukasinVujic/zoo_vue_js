dfe49820742f3c1df4f26fccd743e01fd8e3cb1d
VukasinVujic <vukasinvujic021@gmail.com>
2019-03-12 20:04:16 +0100

    zadatak 5

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,13 +1,29 @@
 <template>
   <!-- <div>{{ animalList }}</div> -->
   <div>
+    <form @submit.prevent="addAnimal">
+      <label for="sort">sort</label>
+      <input v-model="animal.sort" type="text" name="sort">
+      <br>
+
+      <label for="name">Names</label>
+      <input v-model="animal.name" type="text" name="name">
+      <br>
+
+      <label for="date">Date of Birth</label>
+      <input v-model="animal.date" type="text" name="date">
+      <br>
+
+      <button type="submit">add animal</button>
+    </form>
+
     <table>
       <tr>
         <th>Sorts</th>
         <th>Names</th>
         <th>Date of Birth</th>
       </tr>
-      <tr v-for="(animal, index) in animalList" :key="animal">
+      <tr v-for="(animal, index) in animalList" :key="index">
         <td>{{ animal.sort }}</td>
         <td>{{ animal.name }}</td>
         <td>{{ animal.dateOfBirth ? animal.dateOfBirth : 'NEpoznato' }}</td>
@@ -53,7 +69,12 @@ export default {
           name: "leopoold",
           dateOfBirth: "12.02.2003"
         }
-      ]
+      ],
+      animal: {
+        sort: "",
+        name: "",
+        dateOfBirth: ""
+      }
     };
   },
 
@@ -65,6 +86,9 @@ export default {
       const aaaa = this.animalList[index];
       this.animalList.splice(index, 1);
       this.animalList.unshift(aaaa);
+    },
+    addAnimal(animal) {
+      this.animalList.push({ ...this.animal }); // zato sto je objekat
     }
   }
 };
