b7b4088da384b9ddd7db5e079ca7ce12687c08a0
VukasinVujic <vukasinvujic021@gmail.com>
2019-03-12 18:42:09 +0100

    zadatak 1

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,9 +1,55 @@
 <template>
-  <div>dsjklfsdjfdslfjds</div>
+  <!-- <div>{{ animalList }}</div> -->
+  <div>
+    <table>
+      <tr>
+        <th>Sorts</th>
+        <th>Names</th>
+        <th>Date of Birth</th>
+      </tr>
+      <tr v-for="animal in animalList" :key="animal">
+        <td>{{ animal.sort }}</td>
+        <td>{{ animal.name }}</td>
+        <td>{{ animal.dateOfBirth }}</td>
+      </tr>
+    </table>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      animalList: [
+        {
+          sort: "dog",
+          name: "lesi",
+          dateOfBirth: "12.02.1987"
+        },
+        {
+          sort: "cat",
+          name: "Garfild",
+          dateOfBirth: "12.02.1997"
+        },
+        {
+          sort: "parrot",
+          name: "gggg",
+          dateOfBirth: "12.02.2007"
+        },
+        {
+          sort: "pig",
+          name: "oink",
+          dateOfBirth: "12.02.1945"
+        },
+        {
+          sort: "hamster",
+          name: "leopoold",
+          dateOfBirth: "12.02.2003"
+        }
+      ]
+    };
+  }
+};
 </script>
 
 <style>
